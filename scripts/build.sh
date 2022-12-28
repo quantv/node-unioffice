d5157327bb89e115701c58492586090a973fac09
Quantv <quantv@live.com>
2022-12-28 10:17:08 +0700

    fix: build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,6 @@ set -e
 
 go build -buildmode=c-shared -ldflags="-s -w" -o "include/libspreadsheet.so" src-go/lib.go src-go/funcs.go
 
-#go build -buildmode=c-shared -o "include/libspreadsheet.so" src-go/lib.go
+#go build -buildmode=c-shared -o "include/libspreadsheet.so" src-go/lib.go src-go/funcs.go
 #ldflags="-s -w"
 npm install
